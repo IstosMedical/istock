2a2d50694165d65d1274e20665ef0dc1219b8746
IstosMedical <mazhar@istos.in>
2025-08-21 17:16:09 +0530

    Delete scripts directory

--- scripts/sw.js
+++
@@ -1,8 +0,0 @@
-// scripts/sw.js
-self.addEventListener('install', () => {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
